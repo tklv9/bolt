39dc56310b57a1e80ea4beaa50cb474830aadd31
LAPTOP-4HRG8AHF\にょっき <bamboolv9@gmail.com>
2017-07-25 04:44:53 +0900

    Bolt初回レビュー

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(".bar-container").click(function () {
+  $(".bar-container").toggle();
+  $(".contact-btn-wrapper").toggle();
+});
+
+$(".contact-btn-wrapper").click(function () {
+  $(".bar-container").toggle();
+  $(".contact-btn-wrapper").toggle();
+});
