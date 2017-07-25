bb5dbd1b418b4ed39bd07a4c42656735d373dd80
LAPTOP-4HRG8AHF\にょっき <bamboolv9@gmail.com>
2017-07-25 16:45:50 +0900

    レビュー後修正1回目

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
 $(".bar-container").click(function () {
-  $(".bar-container").toggle();
-  $(".contact-btn-wrapper").toggle();
-});
-
-$(".contact-btn-wrapper").click(function () {
-  $(".bar-container").toggle();
-  $(".contact-btn-wrapper").toggle();
+    $(".contact-btn-wrapper").toggle();
 });
